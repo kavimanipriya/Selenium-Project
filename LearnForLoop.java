c27b4f78085644c2072e4e605113bf053942b399
kavimanipriya <96640820+kavimanipriya@users.noreply.github.com>
2021-12-25 21:38:16 +0530

    Add files via upload

---
+++ LearnForLoop.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class LearnForLoop {
+
+	public static void main(String[] args) {
+		//to Print 1 to 10- same code multiple time-for loop
+		//initialize;condition;increment/decrement
+		
+		for(int i=1;i<=50;i++) {
+			System.out.println(i);
+		}
+			
+		
+
+	}
+
+}
