9ea4bb7a6206e65f929c0d508bb65a95df023a96
kavimanipriya <96640820+kavimanipriya@users.noreply.github.com>
2022-01-05 21:57:35 +0530

    Add files via upload

---
+++ week2/day2/Assignment3.java
@@ -0,0 +1,38 @@
+package week2.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		// 1.  URL "https://acme-test.uipath.com/login"
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://acme-test.uipath.com/login");
+		driver.manage().window().maximize();
+
+		// 2. Enter email as "kumar.testleaf@gmail.com"
+		driver.findElement(By.id("email")).sendKeys("kumar.testleaf@gmail.com");
+
+		// 3. Enter Password as "leaf@12"
+		driver.findElement(By.id("password")).sendKeys("leaf@12");
+
+		// 4. Click login button
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+		// 5. Get the title of the page and print
+		System.out.println("Title of the page is " + driver.getTitle());
+
+		// 6. Click on Log Out
+		String logoutButton = "//form[@id='logout-form']/following-sibling::li/a[text()='Log Out']";
+		driver.findElement(By.xpath(logoutButton)).click();
+
+		// 7. Close the browser (use -driver.close())
+		driver.close();
+
+	}
+
+}
