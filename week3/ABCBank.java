83ac8d688376fd467d3d32cfba288daa7314d045
kavimanipriya <96640820+kavimanipriya@users.noreply.github.com>
2022-01-18 17:51:38 +0530

    Add files via upload

---
+++ week3/ABCBank.java
@@ -0,0 +1,15 @@
+package week3.day2;
+
+public abstract class ABCBank implements  RBI{
+
+	public void minimumBalance() {
+		
+		
+	}
+public void  agriLoan()
+{
+	
+}
+public abstract void eductionLoan();	
+	}
+
